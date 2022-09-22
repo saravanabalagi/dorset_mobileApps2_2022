53ab97e40f320b1376d558577a4dc1687b9b1f80
Saravanabalagi <saravanabalagi@hotmail.com>
2022-09-22 15:06:21 +0100

    Add maps into MainActivity

--- app/src/main/java/com/saravanabalagi/dorset_mobile_app_2/MainActivity.kt
+++ app/src/main/java/com/saravanabalagi/dorset_mobile_app_2/MainActivity.kt
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.commit
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.saravanabalagi.dorset_mobile_app_2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val rootView = binding.root
         setContentView(rootView)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         binding.helloWorldTextView.text = getString(R.string.hello_android)
         Log.i("MainActivity", binding.helloWorldTextView.text.toString())
@@ -25,5 +35,14 @@ class MainActivity : AppCompatActivity() {
                 }
             })
         }
+    }
+
+    override fun onMapReady(gm: GoogleMap) {
+        val sydney = LatLng(-34.0, 151.0)
+        gm.addMarker(
+            MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        gm.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
