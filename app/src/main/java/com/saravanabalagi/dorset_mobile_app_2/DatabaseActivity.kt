037f95c6f6d8399a559f1dd0bfd046780d135a7b
Saravanabalagi <saravanabalagi@hotmail.com>
2022-10-13 12:42:16 +0100

    Delayed dbOps to debug using Database Inspector

--- app/src/main/java/com/saravanabalagi/dorset_mobile_app_2/DatabaseActivity.kt
+++ app/src/main/java/com/saravanabalagi/dorset_mobile_app_2/DatabaseActivity.kt
@@ -2,20 +2,21 @@ package com.saravanabalagi.dorset_mobile_app_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.room.*
 import com.saravanabalagi.dorset_mobile_app_2.db.AppDatabase
 import com.saravanabalagi.dorset_mobile_app_2.models.User
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class DatabaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_database)
 
-        Thread {
+        val scheduledExecutor = Executors.newSingleThreadScheduledExecutor()
+        scheduledExecutor.schedule({
             dbOps()
-        }.start()
-
+        }, 15, TimeUnit.SECONDS)
     }
 
     private fun dbOps() {
@@ -27,30 +28,30 @@ class DatabaseActivity : AppCompatActivity() {
         val userDao = db.userDao()
 
         // Insert new user
-        userDao.insert(User(1, "Some name 1"))
-        userDao.insert(User(2, "Some name 2"))
-
-        val usersToInsert = listOf(
-            User(3, "Some name 3"),
-            User(4, "Some name 4"),
-            User(5, "Some name 5"),
-            User(6, "Some name 6"),
-        )
-        userDao.insertList(usersToInsert)
+//        userDao.insert(User(1, "Some name 1"))
+//        userDao.insert(User(2, "Some name 2"))
+//
+//        val usersToInsert = listOf(
+//            User(3, "Some name 3"),
+//            User(4, "Some name 4"),
+//            User(5, "Some name 5"),
+//            User(6, "Some name 6"),
+//        )
+//        userDao.insertList(usersToInsert)
 
         // Update user
         val userToUpdate = User(3, "Updated name 3")
         userDao.update(userToUpdate)
 
         // Delete user
-        val userToDelete = User(2, null)
-        userDao.delete(userToDelete)
-
-        // retrieve all users
-        val users: List<User> = userDao.getAll()
-        users.map {
-            Log.i("DatabaseAct", it.toString())
-        }
+//        val userToDelete = User(2, null)
+//        userDao.delete(userToDelete)
+//
+//        // retrieve all users
+//        val users: List<User> = userDao.getAll()
+//        users.map {
+//            Log.i("DatabaseAct", it.toString())
+//        }
     }
 }
 
