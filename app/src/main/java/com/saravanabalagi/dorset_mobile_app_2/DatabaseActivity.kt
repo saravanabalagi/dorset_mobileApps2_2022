b0711d2531deea4229cde56cfba40f551270dcf5
Saravanabalagi <saravanabalagi@hotmail.com>
2022-10-20 13:30:09 +0100

    Firebase auth sign up and sign in with email and password

--- app/src/main/java/com/saravanabalagi/dorset_mobile_app_2/DatabaseActivity.kt
+++ app/src/main/java/com/saravanabalagi/dorset_mobile_app_2/DatabaseActivity.kt
@@ -3,63 +3,71 @@ package com.saravanabalagi.dorset_mobile_app_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.saravanabalagi.dorset_mobile_app_2.databinding.ActivityDatabaseBinding
 
 class DatabaseActivity : AppCompatActivity() {
     val TAG = "DatabaseActivity"
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityDatabaseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_database)
+        binding = ActivityDatabaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val db = Firebase.firestore
-        val usersCollection = db.collection("users")
-
-        // Create a new user with a first and last name
-//        val user1 = User(1,"Ada1", "Lovelace", 1915)
-//        val user2 = User(2,"Ada2", "Lovelace", 1915)
-//        val user3 = User(3,"Ada3", "Lovelace", 1915)
-//        val user4 = User(4,"Ada4", "Lovelace", 1915)
-//        val user5 = User(5,"Ada5", "Lovelace", 1915)
-//
-//        arrayOf(user1, user2, user3, user4, user5).map { user ->
-//            val name = "${user.fname} ${user.lname}"
-//            usersCollection.document(user.id.toString())
-//                .set(user)
-//                .addOnSuccessListener {
-//                    Log.d(TAG, "User $name added with ID: ${user.id}")
-//                }
-//                .addOnFailureListener { e ->
-//                    Log.w(TAG, "Error adding user $name", e)
-//                }
-//        }
-
-        // Update and delete ops
-//        usersCollection.document("1").update("last", "Updated lastname")
-//        usersCollection.document("5").delete()
-//            .addOnSuccessListener {
-//                Log.i(TAG, "Delete user 5 succeeded")
-//            }
-//            .addOnFailureListener {
-//            Log.i(TAG, "Delete user 5 failed")
-//        }
-
-        // Get
-        val userId = 3
-        usersCollection.document(userId.toString()).get().addOnSuccessListener {
-            val user = it.toObject<User>()
-            if (user != null)
-                Log.i(TAG, user.fname)
-            else Log.e(TAG, "User $userId is null")
+        auth = Firebase.auth
+        binding.signup.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            createUser(email, password)
+        }
+        
+        binding.signin.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            authUser(email, password)
         }
     }
+
+    private fun createUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+//                    updateUI(null)
+                }
+            }
+    }
+
+
+    private fun authUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+//                    updateUI(null)
+                }
+            }
+    }
+
 }
-
-class User(
-    var id: Int = -1,
-    var fname: String = "",
-    var lname: String = "",
-    var born: Int = -1,
-)
-
